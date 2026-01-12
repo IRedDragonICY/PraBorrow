c180742d95e6a8289cb99ed28810278f99573877
IRedDragonICY <hendik.suwoto@gmail.com>
2026-01-12 17:01:51 +0700

    chore: cleanup debug logs and temporary files

--- debug_lease.ps1
+++
@@ -1,2 +0,0 @@
-$ErrorActionPreference = "Stop"
-cargo check --tests -p praborrow-lease 2>&1 | Out-File -Encoding utf8 debug_lease.txt
--- debug_panic.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = "Stop"
-$env:RUST_BACKTRACE = "1"
-cargo test -p praborrow-defense 2>&1 | Out-File -Encoding utf8 debug_panic.txt
